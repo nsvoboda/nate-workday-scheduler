3dbb833b38fcd3d88bed1d3246b3fd9255ebd902
Nate <natesvoboda64@gmail.com>
2022-01-05 18:58:03 -0500

    added HTML elements for each hour as well as initial JS variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// Moment.js
+var currentDate = moment().format('dddd') + "" + moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// HTML for each hour in a day
+var nineAm = document.querySelector("#nine-am");
+var tenAm = document.querySelector("#ten-am");
+var elevenAm = document.querySelector("#eleven-am");
+var twelvePm = document.querySelector("#twelve-pm");
+var onePm = document.querySelector("#one-pm");
+var twoPm = document.querySelector("#two-pm");
+var threePm = document.querySelector("#three-pm");
+var fourPm = document.querySelector("#four-pm");
+var fivePm = document.querySelector("#five-pm");
+var sixPm = document.querySelector("#six-pm");

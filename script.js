a4fde1c656ad301577ec2b2463bd945454b2ba74
Nate <natesvoboda64@gmail.com>
2022-01-05 19:44:50 -0500

    modified id tags for html and JS

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 var currentDate = moment().format('dddd') + " " + moment().format('MMMM Do YYYY, h:mm:ss a');
 
 // HTML for each hour in a day
-var nineAm = document.querySelector("#nine-am");
-var tenAm = document.querySelector("#ten-am");
-var elevenAm = document.querySelector("#eleven-am");
-var twelvePm = document.querySelector("#twelve-pm");
-var onePm = document.querySelector("#one-pm");
-var twoPm = document.querySelector("#two-pm");
-var threePm = document.querySelector("#three-pm");
-var fourPm = document.querySelector("#four-pm");
-var fivePm = document.querySelector("#five-pm");
-var sixPm = document.querySelector("#six-pm");
+var nineAm = document.querySelector("#9am");
+var tenAm = document.querySelector("#10am");
+var elevenAm = document.querySelector("#11am");
+var twelvePm = document.querySelector("#12pm");
+var onePm = document.querySelector("#13pm");
+var twoPm = document.querySelector("#14pm");
+var threePm = document.querySelector("#15pm");
+var fourPm = document.querySelector("#16pm");
+var fivePm = document.querySelector("#17pm");
+var sixPm = document.querySelector("#18pm");
 
 var hour = moment().hours();
 var userInput;
@@ -56,3 +56,25 @@ function initPage(){
     var init6 = JSON.parse(localStorage.getItem("06:00 pm"));
     sixPm.val(init6);
 }
+
+function background() {
+    $(".form-control").each(function(){
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        console.log(timeTest);
+        console.log(hour);
+
+        if (hour > timeTest){
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+}
+
+$(document).ready(function(){
+    initPage();
+    background();
+})
